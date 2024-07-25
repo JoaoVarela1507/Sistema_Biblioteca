d066cb91de66684756df76f7d675aaded5aa8799
João Varela <115510721+JoaoVarela1507@users.noreply.github.com>
2024-07-25 02:16:01 +0000

    Ajuste do cod de Ana e brena

--- biblioteca.js
+++ biblioteca.js
@@ -470,28 +470,20 @@ function mostrarLivrosAlugados(nomeUsuario){
     if (contador === 0){
         console.log("Não existem livros alugados no momento.\n");
     }
-//função de chamar 2 caminhos, Brenna e Ana
-
-
-    menuprincipal(nomeUsuario);
- 
-}
 
     let proximaAcao = prompt("Deseja voltar para o menu principal ou sair? ( 1 para menu principal / 2 para sair): ");
     proximaAcao = parseInt(proximaAcao);
 
     if (proximaAcao === 1) {
-        menuprincipal();
+        menuprincipal(nomeUsuario);
     } else if (proximaAcao === 2) {
         console.log("Obrigado por usar a aplicação! Até a próxima.");
         process.exit(0);
-
     } else {
         console.log("Opção inválida, retornando ao menu principal.");
-        menuprincipal();
-        
+        menuprincipal(nomeUsuario);
     }
-
+}
 
 function devolverLivros(nomeUsuario) {
     texto =`\n    ▒█▀▀█ ▀█▀ ▒█▀▀█ ▒█░░░ ▀█▀ ▒█▀▀▀█ ▀▀█▀▀ ▒█▀▀▀ ▀▄▒▄▀ 
